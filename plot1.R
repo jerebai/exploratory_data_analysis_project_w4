04c30d2f483ce2face1e0a2569272d0b51e2de5f
jerebai <38840065+jerebai@users.noreply.github.com>
2018-05-28 21:59:22 +0100

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,14 @@
+## This first line will likely take a few seconds. Be patient!
+if(!exists("NEI")){
+  NEI <- readRDS("./data/summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+  SCC <- readRDS("./data/Source_Classification_Code.rds")
+}
+# part 2 of this have total emissions decreased?
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+
+png('plot1.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
+dev.off()
